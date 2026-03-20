8171509210a34de89f33265c7c8326cdb266fe70
tothattila05 <tothattila05@gmail.com>
2026-03-20 19:25:57 +0100

    Modified solutions

--- solutions/20200305d/happy_new_year.c
+++ solutions/20200305d/happy_new_year.c
@@ -2,6 +2,8 @@
 
 int main(void)
 {
+    printf("--- Happy New Year V1 ---\n\n");
+
     printf("%d\n", '-' * '-' + ('B' - 'A'));
     return 'A' - 'A';
 }
--- solutions/20200305d/happy_new_year_v2.c
+++ solutions/20200305d/happy_new_year_v2.c
@@ -2,6 +2,8 @@
 
 int main(void)
 {
+    printf("--- Happy New Year V2 ---\n\n");
+
     printf("%c%c%c%c\n", 'R' - ' ', 'P' - ' ', 'R' - ' ', 'V' - ' ');
     return 'A' - 'A';
 }
